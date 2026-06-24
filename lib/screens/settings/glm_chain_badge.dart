dd6d9bf80bd3c98a0a53b151ffa72224c9d222c8
tught3 <tught3@gmail.com>
2026-06-25 00:13:05 +0900

    feat: PlanFlow 설정 GLM 체인 배지 위젯 추가

---
+++ lib/screens/settings/glm_chain_badge.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class GlmChainBadge extends StatelessWidget {
+  const GlmChainBadge({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text('GLM Chain V1');
+  }
+}
